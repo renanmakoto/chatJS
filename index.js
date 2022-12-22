31fd1e2571d62dfaef5ab67ab58670d58f9ecf7d
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2022-12-22 08:29:30 -0300

    Add files via upload
    Chat rooms were created to have separate conversation.

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ const app = express()
 const path = require('path')
 const socketIo = require('socket.io')
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/room1', express.static(path.join(__dirname, 'public')))
+app.use('/room2', express.static(path.join(__dirname, 'public')))
 
 const server = app.listen(3000, () => {
     console.log("Server running")
 })
 
-const messages = []
+const messages = {
+    room1: [],
+    room2: []
+}
 
 const io = socketIo(server)
 
-io.on('connection', (socket) => {
+const room1 = io.of('/room1').on('connection', (socket) => {
     console.log('new connection')
-    socket.emit('update_messages', messages)
+    socket.emit('update_messages', messages.room1)
 
     socket.on('new_message', (data) => {
-        messages.push(data.msg)
-
-        io.emit('update_messages', messages)
+        messages.room1.push(data)
+        console.log(messages)
+        room1.emit('update_messages', messages.room1)
     })
+})
 
+const room2 = io.of('/room2').on('connection', (socket) => {
+    console.log('new connection')
+    socket.emit('update_messages', messages.room2)
+
+    socket.on('new_message', (data) => {
+        messages.room2.push(data)
+        console.log(messages)
+        room2.emit('update_messages', messages.room2)
+    })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,41 +1,41 @@
-const socket = io('http://localhost:3000')
+const chatRoom = window.location.pathname.replace(/\//g, '')
+const socket = io(`http://localhost:3000/${chatRoom}`)
+
+let user = null
 
 socket.on('update_messages', (messages) => {
-
     updateMessagesOnScreen(messages)
-
 })
 
 function updateMessagesOnScreen(messages) {
-
     const div_msgs = document.querySelector('#msgs')
-
     let msgs_list = '<ul>'
-
     messages.forEach(message => {
-
-        msgs_list += `<li>${message}</li>`
+        msgs_list += `<li> ${message.user}: ${message.msg}</li>`
     })
-
     msgs_list += '</ul>'
-
     div_msgs.innerHTML = msgs_list
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
     const form = document.querySelector('#msgForm')
-
     form.addEventListener('submit', (theEvent) => {
-
         theEvent.preventDefault()
+        if (!user) {
+            alert('Type an username, please.')
+            return
+        }
 
         const message = document.forms['msgFormName']['msgInput'].value
-
         document.forms['msgFormName']['msgInput'].value = ''
-
-        socket.emit('new_message', { msg: message })
-
+        socket.emit('new_message', { user: user, msg: message })
         console.log(message)
+    })
+
+    const userForm = document.querySelector('#userForm')
+    userForm.addEventListener('submit', (theEvent) => {
+        theEvent.preventDefault()
+        user = document.forms['userFormName']['userInput'].value
+        userForm.parentNode.removeChild(userForm)
     })
 })
